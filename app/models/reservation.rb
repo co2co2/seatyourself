578dc4c8f56b2a3eb6c9aabc12d5e05ea1471181
co2co2 <coco8287@hotmail.com>
2017-12-05 15:28:26 -0500

    reservation validations done

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,11 +3,45 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
 
   validates :date, :party_size, presence: true
-  validates :party_size, numericality: {message: "%{value} has to be a number!"}, unless: :restaurant_at_capacity?
-end
+  validates :party_size, numericality: {only_integer: true}
+  validate :time_cannot_be_earlier_than_now
+  validate :restaurant_at_capacity
+  validate :within_open_hours
 
 private
-  def restaurant_at_capacity?
-    if self.party_size > self.restaurant.capacity
+
+  def time_cannot_be_earlier_than_now
+    if self.time_slot > Time.now
+      return true
+    else
+      errors.add(:time_slot, "You cannot go back in time. ")
     end
   end
+
+  def within_open_hours
+    if self.time_slot > self.restaurant.open_hour && self.time_slot < self.restaurant.close_hour
+      return true
+    else
+      errors.add(:time_slot,"Sorry,the restaurant isn't open at that time.")
+    end
+  end
+
+  def seat_left
+    seat_available = self.restaurant.capacity
+    upper = self.time_slot + 120.minutes
+    lower = self.time_slot - 120.minutes
+    current_reservations = Reservation.where( "time_slot > ? && time_slot < ? ",lower , upper )
+
+    current_reservations.each do |r|
+      seat_available -= r.party_size
+    end
+  end
+
+  def restaurant_at_capacity
+    if self.party_size  <  self.seat_left
+      return true
+    else
+      errors.add(:party_size, "The restaurant is full at that timeslot.")
+    end
+  end
+end
