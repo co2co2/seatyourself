feb959ab32897be16325ae8dd6dc55153cae07af
Lena <fatmeow@gmail.com>
2017-12-07 00:37:31 -0500

    Styled Restaurant show page and edit page for reservation

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20171207022033) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "points"
   end
 
 end
